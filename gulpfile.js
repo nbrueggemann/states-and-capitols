15dfa479fe18a0767d965cf4d6a2bd54d3beaecb
AVWORLD\nick8770 <nbrueggemann@esri.com>
2017-02-19 21:16:09 -0600

    Comment

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ watch = require('gulp-watch');
 var appDir = "./wab/server/apps/2";
 
 gulp.task('watch', function () {
-    // Watch entire src directory
+    // Watch entire src directory.  When something changes copy it to the apps dir.
     return watch('./src/**/*.*', { ignoreInitial: false })
         .pipe(gulp.dest(appDir));
 });
