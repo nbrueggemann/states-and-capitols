e3914c3caa94ac40122e0cccdfd16fef6aabaa9a
AVWORLD\nick8770 <nbrueggemann@esri.com>
2017-02-19 22:08:57 -0600

    Added a development config

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 watch = require('gulp-watch');
 
-var appDir = "./wab/server/apps/2";
+var appDir = "./wab/client/stemapp";
 
 gulp.task('watch', function () {
     // Watch entire src directory.  When something changes copy it to the apps dir.
